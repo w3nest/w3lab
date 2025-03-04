7fe22d860b2dc3a1474050f8e4a02183b8ba970a
youwol-guillaume <reinisch.gui@gmail.com>
2025-03-04 14:15:06 +0100

    👽️ [project] Sync.  `FailuresView` w/ w3nest API (& simplify)

--- src/app/projects/failures.view.ts
+++ src/app/projects/failures.view.ts
@@ -1,14 +1,9 @@
 import { Local } from '@w3nest/http-clients'
 import { ChildrenLike, replace$, VirtualDOM } from 'rx-vdom'
 import { AppState } from '../app-state'
-import { parseMd, Router } from 'mkdocs-ts'
+import { Router } from 'mkdocs-ts'
 import { ExpandableGroupView } from '../common/expandable-group.view'
 import { HdPathBookView } from '../common'
-
-type Failures =
-    | Local.Projects.FailurePipelineNotFound[]
-    | Local.Projects.FailureDirectoryNotFound[]
-    | Local.Projects.FailureImportException[]
 
 export class FailuresView implements VirtualDOM<'div'> {
     public readonly tag = 'div'
@@ -25,109 +20,62 @@ export class FailuresView implements VirtualDOM<'div'> {
         this.children = replace$({
             policy: 'replace',
             source$: appState.projectsState.projectsFailures$,
-            vdomMap: (failures) => [
-                new FailuresCategoryView({
-                    appState: appState,
-                    failures: failures.importExceptions.filter((error) =>
-                        prefix ? error.path.startsWith(prefix) : true,
-                    ),
-                    title: 'Import Failures',
-                }),
-                new FailuresCategoryView({
-                    appState: appState,
-                    failures: failures.directoriesNotFound.filter((error) =>
-                        prefix ? error.path.startsWith(prefix) : true,
-                    ),
-                    title: 'Directory Not Found Failures',
-                }),
-                new FailuresCategoryView({
-                    appState: appState,
-                    failures: failures.pipelinesNotFound.filter((error) =>
-                        prefix ? error.path.startsWith(prefix) : true,
-                    ),
-                    title: 'Pipeline Not Found Failures',
-                }),
-            ],
+            vdomMap: (failures) => {
+                return failures
+                    .filter((failure) =>
+                        prefix ? failure.path.startsWith(prefix) : true,
+                    )
+                    .map((failure) => {
+                        return new FailureView({ failure, appState })
+                    })
+            },
         })
     }
 }
 
-class FailuresCategoryView implements VirtualDOM<'div'> {
+class FailureView extends ExpandableGroupView {
     public readonly tag = 'div'
     public readonly children: ChildrenLike
 
     constructor({
         appState,
-        failures,
-        title,
+        failure,
     }: {
         appState: AppState
-        failures: Failures
-        title: string
+        failure: Local.Projects.Failure
     }) {
-        if (failures.length === 0) {
-            return
-        }
-        this.children = [
-            parseMd({
-                src: `
-### ${title}
-
-<failures></failures>
-            `,
-                router: undefined,
-                views: {
-                    failures: () => {
-                        return {
-                            tag: 'div',
-                            children: failures.map(
-                                (failure: Local.Projects.Failure) => ({
-                                    tag: 'div' as const,
-                                    class: 'my-4',
-                                    children: [
-                                        new ExpandableGroupView({
-                                            title: {
-                                                tag: 'div',
-                                                style: {
-                                                    maxWidth: '75%',
-                                                },
-                                                children: [
-                                                    new HdPathBookView({
-                                                        path: failure.path,
-                                                        appState,
-                                                        type: 'folder',
-                                                    }),
-                                                ],
-                                            },
-                                            icon: 'fas fa-times fv-text-error',
-                                            content: () => {
-                                                return {
-                                                    tag: 'pre',
-                                                    children: [
-                                                        {
-                                                            tag: 'div',
-                                                            class: 'pt-2 px-2 text-start overflow-auto fv-text-error ',
-                                                            style: {
-                                                                whiteSpace:
-                                                                    'pre-wrap',
-                                                            },
-                                                            innerText:
-                                                                (failure[
-                                                                    'traceback'
-                                                                ] as string) ??
-                                                                failure.message,
-                                                        },
-                                                    ],
-                                                }
-                                            },
-                                        }),
-                                    ],
-                                }),
-                            ),
-                        }
-                    },
+        super({
+            title: {
+                tag: 'div',
+                style: {
+                    maxWidth: '75%',
                 },
-            }),
-        ]
+                children: [
+                    new HdPathBookView({
+                        path: failure.path,
+                        appState,
+                        type: 'folder',
+                    }),
+                ],
+            },
+            icon: 'fas fa-times fv-text-error',
+            content: () => {
+                return {
+                    tag: 'pre',
+                    children: [
+                        {
+                            tag: 'div',
+                            class: 'pt-2 px-2 text-start overflow-auto fv-text-error ',
+                            style: {
+                                whiteSpace: 'pre-wrap',
+                            },
+                            innerText:
+                                (failure['traceback'] as string) ??
+                                failure.message,
+                        },
+                    ],
+                }
+            },
+        })
     }
 }
