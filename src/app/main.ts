7c723cdf2b091b39f1a8feee3d3e4003bb6cea19
youwol-guillaume <reinisch.gui@gmail.com>
2025-04-24 18:05:19 +0200

    💄 Plug new loading screen

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,23 +1,26 @@
 import './style.css'
 export {}
-import * as webpmClient from '@w3nest/webpm-client'
+import { install, LoadingScreen } from '@w3nest/webpm-client'
+import pkgJson from '../../package.json'
 
-import { setup } from '../auto-generated'
+const mkdocsVersion = pkgJson.webpm.dependencies['mkdocs-ts']
 
-const mkdocsVersion = setup.runTimeDependencies.externals['mkdocs-ts']
-
-await setup.installMainModule({
-    cdnClient: webpmClient,
-    installParameters: {
-        css: [
-            'bootstrap#5.3.3~bootstrap.min.css',
-            'bootstrap#5.3.3~bootstrap-utilities.min.css',
-            'highlight.js#11.2.0~styles/default.css',
-            'fontawesome#5.12.1~css/all.min.css',
-            `mkdocs-ts#${mkdocsVersion}~assets/mkdocs-light.css`,
-        ],
-        displayLoadingScreen: true,
-    },
+const loadingScreen = new LoadingScreen({
+    logo: '../assets/favicon.svg',
+    name: pkgJson.name,
+    description: pkgJson.description,
 })
 
+await install({
+    esm: [`${pkgJson.name}#${pkgJson.version}`],
+    css: [
+        'bootstrap#5.3.3~bootstrap.min.css',
+        'bootstrap#5.3.3~bootstrap-utilities.min.css',
+        'highlight.js#11.2.0~styles/default.css',
+        'fontawesome#5.12.1~css/all.min.css',
+        `mkdocs-ts#${mkdocsVersion}~assets/mkdocs-light.css`,
+    ],
+    onEvent: (ev) => loadingScreen.next(ev),
+})
+loadingScreen.done()
 await import('./on-load')
