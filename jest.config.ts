43e13527a44ee5ff881064b96523baf938d715dd
youwol-guillaume <reinisch.gui@gmail.com>
2025-09-06 09:26:36 +0200

    🔧 sync. config. files

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,15 @@
 import { Config } from 'jest'
+import type { Config as JestConfig } from '@jest/types'
+
+const reporters: JestConfig.ReporterConfig[] = [
+    ['default', {}], // keep default console output
+    [
+        'jest-junit',
+        {
+            outputDirectory: 'tooling/jest',
+        },
+    ],
+]
 
 const jestConfig: Config = {
     transform: { '^.+\\.tsx?$': ['ts-jest', {}] },
@@ -6,5 +17,6 @@ const jestConfig: Config = {
     testEnvironment: 'jsdom',
     modulePathIgnorePatterns: ['<rootDir>/.w3nest', '<rootDir>/dist'],
     coverageDirectory: 'tooling/jest',
+    reporters,
 }
 export default jestConfig
